36eb0d0fadc5c38aa2329a7516035e96c435bc06
sanikani <sanikani@naver.com>
2023-05-31 01:09:17 +0900

    2023-05-30 java study

---
+++ thisisjava/src/ch15/sec05/exam03/ComparableExample.java
@@ -0,0 +1,18 @@
+package ch15.sec05.exam03;
+
+import java.util.TreeSet;
+
+public class ComparableExample {
+    public static void main(String[] args) {
+        TreeSet<Person> treeSet = new TreeSet<>();
+
+        treeSet.add(new Person("홍길동",45));
+        treeSet.add(new Person("김자바",25));
+        treeSet.add(new Person("박지원",31));
+
+        //객체를 하나씩 가져오기
+        for(Person person : treeSet){
+            System.out.println(person.name + ":" + person.age);
+        }
+    }
+}
