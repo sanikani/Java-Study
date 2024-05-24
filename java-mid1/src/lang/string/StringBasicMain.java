1a5b647e23dd00894eef6e006293ccca893889cc
sanikani <sanikani@naver.com>
2024-05-24 18:18:33 +0900

    String 기본, 불변객체

---
+++ java-mid1/src/lang/string/StringBasicMain.java
@@ -0,0 +1,9 @@
+package lang.string;
+
+public class StringBasicMain {
+
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = new String("hello");
+    }
+}
